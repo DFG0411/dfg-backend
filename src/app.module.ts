0ed2eacd3b179e45c607fad13cd9c026f8261fbf
Zhaohui Gong <gongzhaohui@gmail.com>
2020-11-09 15:33:43 +0800

    double import

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,6 @@ import { AuthModule } from './admin/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProvinceModule } from './admin/province/province.module';
 import { CityModule } from './admin/city/city.module';
-import { ProvinceModule } from './admin/province/province.module';
 // import { PassportModule } from '@nestjs/passport';
 // import { JwtModule } from '@nestjs/jwt';
 // import { JwtStrategy } from './admin/auth/passport/jwt.strategy';
